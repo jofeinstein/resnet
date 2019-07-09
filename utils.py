b12dad47a4459fc681a4b87a9dfda60c4eb16a8e
JoJo Feinstein <jofeinstein@8c-85-90-54-05-15.wlan.lsu.edu>
2019-07-09 15:44:04 -0500

    added trainer with no validation

--- utils.py
+++ utils.py
@@ -86,6 +86,71 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
     return model, val_acc_history, val_loss_history, train_acc_history, train_loss_history
 
 
+def train_model_no_validation(model, dataloaders, criterion, optimizer, num_epochs=25):
+    since = time.time()
+
+    train_acc_history = []
+    train_loss_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        # Each epoch has only a training phase
+
+        model.train()  # Set model to training mode
+
+        running_loss = 0.0
+        running_corrects = 0
+
+        # Iterate over data.
+        for inputs, labels in dataloaders:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            # track history if only in train
+            with torch.set_grad_enabled(True):
+                # Get model outputs and calculate loss
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                _, preds = torch.max(outputs, 1)
+
+                # backward + optimize only if in training phase
+                loss.backward()
+                optimizer.step()
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+        epoch_loss = running_loss / len(dataloaders.dataset)
+        epoch_acc = running_corrects.double() / len(dataloaders.dataset)
+
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1) + '   Loss: {:.4f}'.format(epoch_loss)
+              + '  Acc: {:.4f}'.format(epoch_acc))
+
+        # deep copy the model
+        if epoch_acc > best_acc:
+            best_acc = epoch_acc
+            best_model_wts = copy.deepcopy(model.state_dict())
+        train_acc_history.append(epoch_acc)
+        train_loss_history.append(epoch_loss)
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60)
+          + '      Best val Acc: {:4f}'.format(best_acc) + '\n')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+
+    return model, train_acc_history, train_loss_history
+
+
 def set_parameter_requires_grad(model, feature_extracting):
     if feature_extracting:
         for param in model.parameters():
