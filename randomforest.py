b59e3f5c49a92ee5891e4cce06f7508614f1f768
JoJo Feinstein <jofeinstein@Mellow-Mike.local>
2019-07-22 08:20:43 -0500

    random forest progress

--- randomforest.py
+++ randomforest.py
@@ -5,12 +5,25 @@ import os
 from os import listdir
 import glob
 
-feature = pickle.load(open('/home/jfeinst/Projects/resnet-files/features/train/1/1hqcB00-3/1hqcB00_blended_XOY+_r0_OO.pickle', 'rb'))
-path = '/home/jfeinst/Projects/resnet-files/features/train/1/*/*.pickle'
+feature = pickle.load(open('/Users/jofeinstein/Desktop/features/train/1/1hqcB00-3/1hqcB00_blended_XOY+_r0_OO.pickle', 'rb'))
+path = '/Users/jofeinstein/Desktop/features/train/1/*/*.pickle'
 #print(feature.size)
-#df = pd.DataFrame(feature)
+data = pd.DataFrame()
+df = pd.DataFrame()
 
+count = 0
+for dillpickle in glob.glob(path):
+    count += 1
+    print(count)
+    if count == 1:
+        feature_vec = pickle.load(open(dillpickle, 'rb'))
+        data = pd.DataFrame(feature_vec)
+    elif count == 100:
+        break
+    else:
+        feature_vec = pickle.load(open(dillpickle, 'rb'))
+        df = pd.DataFrame(feature_vec)
+        data = pd.merge(data, df)
 
-for pickle in glob.glob(path):
-    feature_vec = open(pickle, 'rb')
-    print(type(feature_vec))
+print(df.shape)
+print(data.shape)
