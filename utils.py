752e44e856701c9951d3d8d6dd21a9c824b036d5
jofeinstein <jofeinstein@gmail.com>
2019-07-23 16:27:34 -0500

    added lr scheduler step. added inception trainer.

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ import copy
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, num_classes=2):
+def train_model(model, dataloaders, criterion, optimizer, learning_rate_scheduler, num_epochs=25, num_classes=2):
     since = time.time()
 
     val_acc_history = []
@@ -27,12 +27,15 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, num_cla
     print('Beginning training...')
 
     for epoch in range(num_epochs):
+
+
         time_taken = time.time()
 
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
                 model.train()  # Set model to training mode
+                learning_rate_scheduler.step()
             else:
                 model.eval()  # Set model to evaluate mode
 
@@ -241,3 +244,101 @@ def list_plot(lst, title):
     # plt.legend()
     plt.draw()
     fig.savefig('./log/' + title + '.png', dpi=500)
+
+def train_model_inception(model, dataloaders, criterion, optimizer, learning_rate_scheduler, num_epochs=25, is_inception=False, num_classes=2):
+
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        time_taken = time.time()
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+                learning_rate_scheduler.step()
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+            if phase == 'val':
+                confusion_matrix = torch.zeros(num_classes, num_classes)
+                with torch.no_grad():
+                    for i, (inputs, classes) in enumerate(dataloaders['val']):
+                        inputs = inputs.to(device)
+                        classes = classes.to(device)
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        for t, p in zip(classes.view(-1), preds.view(-1)):
+                            confusion_matrix[t.long(), p.long()] += 1
+                print('Confusion matrix:')
+                print(confusion_matrix)
+
+            time_used = time.time() - time_taken
+            print('Time elapsed: {:.0f}m {:.0f}s'.format(time_used // 60, time_used % 60))
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60)
+              + '      Best val Acc: {:4f}'.format(best_acc))
+        print('-' * 15 + '\n')
+
+        # load best model weights
+        model.load_state_dict(best_model_wts)
+    return model, val_acc_history
