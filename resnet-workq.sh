f1905d809c2fdfa4349dce6ded5e47b26c7f828d
jofeinstein <jofeinstein@gmail.com>
2019-07-23 16:29:56 -0500

    added scripts for qb

--- resnet-workq.sh
+++ resnet-workq.sh
@@ -2,9 +2,8 @@
 #PBS -l nodes=1:ppn=20
 #PBS -l walltime=72:00:00
 #PBS -q workq
-#PBS -N resnet-test
-#PBS -A loni_omics01
+#PBS -N resnet-transfer
+#PBS -A loni_bionoi01
 #PBS -j oe
 
 sleep 100000000000
-
